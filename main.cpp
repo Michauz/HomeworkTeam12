db37332b397791ac1e90ba508d7d31feca113d19
michauz <michauz@ac.sec.ac.il>
2019-11-29 09:03:43 +0200

    Second commit - added instructions

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//This is the first main file
+//homework will be done here
+//Dan is doing the person and the student class
+//Alex is doing Employee class
+//Michael is doing WorkingStudent class
+//Sahar is doing the main class
+//Dont forget to add comments and documentation
+//Dont forget to add easter eggs (;
+#include <iostream>
+int main() {
+	system("pause");
+	return 0;
+}
