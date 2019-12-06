e8f40733f10f3029db5ac3164bd137ded9c8128e
XAlexxelAX <alexla@ac.sce.ac.il>
2019-12-06 17:11:01 +0200

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -4,5 +4,10 @@
 
 class Employee :public Person {
 	float salary;
+public:
+	Employee();
+	Employee(float salary, char* n, long id, int age);
+	void virtual Print()const;
+	int getavg()const;
 	};
 #endif 
