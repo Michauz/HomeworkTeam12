c84801eea8d0cd9ce6d8b0d8c6d6e86e6703d0ef
ShaharBlank <shahabl@ac.sce.ac.il>
2019-12-10 16:27:36 +0200

    Added: classes includes & input from user

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 //Dan is doing the person and the student class
 //Alex is doing Employee class
 //Michael is doing WorkingStudent class
-//Sahar is doing the main class
+//Shahar is doing the main class
 //Dont forget to add comments and documentation
 //Dont forget to add easter eggs (;
 #include <iostream>
+#include "Employee.h"
+#include "Person.h"
+#include "Student.h"
+#include "WorkingStudent.h"
+using namespace std;
 int main() {
+	
+
 	system("pause");
 	return 0;
 }
