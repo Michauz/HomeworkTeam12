adc3f90600d891e1fe392a7c3152a912ac6ae41d
Michauz <michauz@ac.sce.ac.il>
2019-12-10 15:36:33 +0200

    added bool class

--- WorkingStudent.h
+++ WorkingStudent.h
@@ -1,6 +1,15 @@
 #include "Person.h"
 #include "Employee.h"
 #include "Student.h"
+#include <string.h>
 class workingStudent :public Student,public Employee{
+    public:
+    
+    bool same_institute(char *inst){
+        if (strcmp(getinsti(),inst)==0)
+            return true;
+        return false;
+    }
+
 
 };
